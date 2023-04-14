aaeb69fd55925455e7b379fb93516d4b2730d51c
Calvin Post <calvinator08@gmail.com>
2023-04-14 15:31:36 -0500

    module7

--- jobs/app.py
+++ jobs/app.py
@@ -69,7 +69,10 @@ def employer(employer_id):
 @app.route('/employer/<employer_id>/review', methods = ["GET", "POST"])
 def review(employer_id):
     if request.method == 'POST':
-        review, rating, title, status = request.form['review'], request.form['rating'], request.form['title'], request.form['status']
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
         date = datetime.now().strftime("%m/%d/%Y")
         execute_sql(
             'INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
